b9d616e602adcab84315298cb61041e8ea88f6b2
chrisdotgrubb <developer.grubb@gmail.com>
2023-07-11 09:48:55 -0400

    Add Finch detail page

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,4 +7,5 @@ app_name = 'finch'
 urlpatterns = [
 	path('', views.index, name='home'),
 	path('about/', views.about, name='about'),
+	path('finches/<int:pk>/', views.detail, name='about'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Finch
 
@@ -14,3 +14,10 @@ def index(request):
 
 def about(request):
 	return render(request, 'about.html', context={})
+
+def detail(request, pk):
+	context = {
+		'finch': get_object_or_404(Finch, pk=pk),
+	}
+	print(context, pk)
+	return render(request, 'detail.html', context)
